185ae0032e3413bd7a80650d5bcfa28f60669700
tsninjbadgar <tsninjbadgar@gmail.com>
2025-04-04 16:29:08 +0800

    android+

--- app/src/main/java/com/example/translator/data/Option.kt
+++
@@ -1,7 +0,0 @@
-package com.example.translator.data
-
-enum class Option(val value: String) {
-    BOTH("both"),
-    ENGLISH("english"),
-    MONGOLIA("mongolia")
-}
--- app/src/main/java/com/example/translator/ui/navigation/NavigationDestination.kt
+++
@@ -1,9 +0,0 @@
-package com.example.translator.ui.navigation
-
-/**
- * Interface to describe the navigation destinations for the app
- */
-interface NavigationDestination {
-
-    val route: String
-}
